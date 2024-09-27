7d71bf33f5bca9d656b3d2f8fdf67b8936deb300
gabrielabejarano09 <gabrielabejarano09@gmail.com>
2024-09-26 19:11:06 -0500

    added first part of authentication with tokens

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = 3000;
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Routes
 userRoute = require('./Routes/user');
 productRoute = require('./Routes/product');
 orderRoute = require('./Routes/order');
@@ -12,6 +16,48 @@ orderRoute = require('./Routes/order');
 app.use('/users', userRoute);
 app.use('/products', productRoute);
 app.use('/orders', orderRoute);
+
+//Authentication
+
+const accounts =[
+  {
+    "email": "admin@admin.com",
+    "password": "admin"
+  }
+]
+
+app.get('/accounts', authenticateToken, (req, res) => {
+  res.json(accounts.filter(account => account.email === req.user.username ));
+})
+
+app.post('/login', (req, res) => {
+  //Authenticate User
+
+  const email = req.body.email;
+  const user = {username: email};
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({accessToken: accessToken})
+})
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(" ")[1]
+  if(token == null){
+    res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if(err) {
+      return res.sendStatus(403);
+    }
+    req.user =user;
+    next();
+  }
+    
+  )
+}
+
 
 app.use((req, res) => {
   res.status(404).send('Not found');
