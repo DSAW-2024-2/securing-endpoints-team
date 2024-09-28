b3d9b8c649b9b466b638f42d27f372e9ad1dd90d
nicoclo205 <148281570+nicoclo205@users.noreply.github.com>
2024-09-27 19:34:17 -0500

    authorization server was updated

--- AuthServer.js
+++ AuthServer.js
@@ -8,8 +8,26 @@ const jwt = require('jsonwebtoken');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+let refreshTokens = [];
+
 
 //Authentication
+
+app.post('/token', (req, res) => {
+    const refreshToken = req.body.token;
+    if(refreshToken == null) return res.sendStatus(401);
+    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403);
+        const accessToken = generateAccessToken({username: user.username});
+        res.json({accessToken: accessToken});
+    })
+    })  
+
+app.delete('/logout', (req, res) => {
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+    res.sendStatus(204);
+})
 
 app.post('/login', (req, res) => {
   //Authenticate User
@@ -19,7 +37,8 @@ app.post('/login', (req, res) => {
 
   const accessToken = generateAccessToken(user);
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
-  res.json({accessToken: accessToken})
+    refreshTokens.push(refreshToken);
+  res.json({accessToken: accessToken, refreshToken: refreshToken})
 })
 
 function generateAccessToken(user) {
